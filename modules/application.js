cb78b28a85958df84369fb5dea4257830a695ea9
Ulrike Kaiser <ulrike1985@live.de>
2015-02-08 14:07:00 +0100

    Layout and navigation

--- modules/application.js
+++ modules/application.js
@@ -11,6 +11,18 @@ module.exports = function (config, libraries, services) {
     var app = services.app;
 
     app.get('/', function (req, res) {
-        res.render('application.twig', config);
+        res.render('homepage.twig', { application: config.application, active: '/' });
+    });
+
+    app.get('/writeto', function (req, res) {
+        res.render('writeto.twig', { application: config.application, active: '/writeto' });
+    });
+
+    app.get('/cv', function (req, res) {
+        res.render('cv.twig', { application: config.application, active: '/cv' });
+    });
+
+    app.get('/attachements', function (req, res) {
+        res.render('attachements.twig', { application: config.application, active: '/attachements' });
     });
 };
